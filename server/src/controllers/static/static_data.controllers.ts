cf0a0245946638feae0ef51fedcb08cd1d44bc2e
Maximilien <maximilien.philippe@protonmail.com>
2025-03-05 14:45:21 +0100

    add static_data controller

---
+++ server/src/controllers/static/static_data.controllers.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+
+import { GitHubRepository } from "../../types/github.types";
+import staticData from '../../data/static_data.json';
+
+const StaticContoller = {
+    getAllRepository: (_: Request, res: Response) => {
+        res.status(200).json(staticData);
+    },
+
+    findRepositoryWithID: (req: Request, res: Response) => {
+        const repo_id = req.params.reposid;
+    
+        const repo = staticData.find(r => {
+            return r.id === repo_id
+    
+        }) as GitHubRepository;
+    
+        if(repo) {
+            res.status(200).json(repo);
+        }
+        else {
+            res.status(500).json(`repository with id ${repo_id} dosn't exist`);
+        }
+    },
+    
+}
+
+export default StaticContoller;
--- server/src/router/repos/repos.ts
+++ server/src/router/repos/repos.ts
@@ -1,30 +1,14 @@
 import express, { Request, Response } from "express";
-
+import StaticContoller from "../../controllers/static/static_data.controllers";
 import { GitHubRepository } from "../../types/github.types";
 import staticData from '../../data/static_data.json';
 
 const router = express.Router();
 
 // GET Root repos
-router.get("/", (_, res: Response) => {
-    res.status(200).json(staticData);
-});
+router.get("/", StaticContoller.getAllRepository);
 
-router.get("/:reposid", (req: Request, res: Response) => {
-    const repo_id = req.params.reposid;
-
-    const repo = staticData.find(r => {
-        return r.id === repo_id
-
-    }) as GitHubRepository;
-
-    if(repo) {
-        res.status(200).json(repo);
-    }
-    else {
-        res.status(500).json(`repository with id ${repo_id} dosn't exist`);
-    }
-});
+router.get("/:reposid", StaticContoller.findRepositoryWithID);
 
 
 
