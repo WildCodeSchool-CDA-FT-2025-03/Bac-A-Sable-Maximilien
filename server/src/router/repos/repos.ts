2a2fdcf521befc2383852689f43734ce1452a579
Maximilien <maximilien.philippe@protonmail.com>
2025-03-05 14:19:17 +0100

    add router repoid

--- server/src/router/repos/repos.ts
+++ server/src/router/repos/repos.ts
@@ -1,13 +1,31 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 
-import staticData from 'data/static_data.json'
+import { GitHubRepository } from "../../types/github.types";
+import staticData from '../../data/static_data.json';
 
 const router = express.Router();
 
 // GET Root repos
 router.get("/", (_, res: Response) => {
-    res.status(200).send(staticData);
+    res.status(200).json(staticData);
 });
+
+router.get("/:reposid", (req: Request, res: Response) => {
+    const repo_id = req.params.reposid;
+
+    const repo = staticData.find(r => {
+        return r.id === repo_id
+
+    }) as GitHubRepository;
+
+    if(repo) {
+        res.status(200).json(repo);
+    }
+    else {
+        res.status(500).json(`repository with id ${repo_id} dosn't exist`);
+    }
+});
+
 
 
 export default router;
