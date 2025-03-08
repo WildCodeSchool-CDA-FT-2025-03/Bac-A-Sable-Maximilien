8fd9f7bec68bc64a364d0fd0750ee0d3d1ab45b6
Maximilien <maximilien.philippe@protonmail.com>
2025-03-08 21:16:35 +0100

    delete dead code

--- server/src/controllers/middlewares/repository/validation.middlewares.ts
+++ server/src/controllers/middlewares/repository/validation.middlewares.ts
@@ -6,37 +6,6 @@ import { RepositoryFields } from "@/core/repository";
 import data_repo from "@/datas/static_data";
 
 export default {
-  addRepoRequest: (req: Request, res: Response, next: NextFunction) => {
-    const { error } = CheckClientAddRepo.validate(req.body);
-    const data = toGitHubRepository(req.body);
-    res.locals.data = data;
-    
-    if (error) {
-      res.status(422).json(error);
-    } 
-    else {
-      next();
-    }
-  },
-
-  queryParameter: (req: Request, res: Response, next: NextFunction) => {
-    const { error } = CheckQueryRequest.validate(req.query);
-  
-    if (error) {
-      res.status(422).json(error);
-    }
-    else {
-      const fields = req.query.fields as string;
-      res.locals.filters = req.query;
-  
-      if(fields) {
-        res.locals.fields = fields.split(',');
-      }
-  
-      next();
-    }
-  },
-
   getRepositorys: (req: Request, res: Response, next: NextFunction) => {
     const { error } = CheckQueryRequest.validate(req.query);
     const config: GetRepositoryRequest = {}; 
