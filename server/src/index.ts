e2cd40a6e045fb6cdb2b01b6ed7660aef45b720d
Maximilien <maximilien.philippe@protonmail.com>
2025-03-09 21:36:13 +0100

    Replace console logging with Winston logger in server startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import "module-alias/register";
 
 import root from "./router/root";
-
+import logger from "@/services/logger";
 const server_port = process.env.SERVER_PORT || 8000;
 
 const server = express();
@@ -13,5 +13,5 @@ server.use(express.json());
 server.use("/", root);
 
 server.listen(server_port, () => {
-  console.info(`Server running on port ${server_port}`);
+  logger.info(`Server running on port ${server_port}`);
 });
