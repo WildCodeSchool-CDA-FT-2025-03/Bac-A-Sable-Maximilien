7813561463b2959e0f72c502b0e2188377e9c11f
Maximilien <maximilien.philippe@protonmail.com>
2025-03-10 12:16:52 +0100

    @server cors: add type & config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import "dotenv/config";
 import "module-alias/register";
+import cors from "cors";
 
 import root from "./router/root";
 import logger from "@/services/logger";
+
 const server_port = process.env.SERVER_PORT || 8000;
 
 const server = express();
+
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 server.use(express.json());
 
