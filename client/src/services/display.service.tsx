ddbcffa409490f8690d48946593767916649a3da
Maximilien <maximilien.philippe@protonmail.com>
2025-03-14 09:24:01 +0100

    delete code

--- client/src/services/display.service.tsx
+++ client/src/services/display.service.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { DisplayType } from '@/components/ToolsBar/ToolsBar';
+// import { DisplayType } from '@/components/ToolsBar/ToolsBar';
 
 const useDisplay = () => {
-    const [display, setDisplay] = useState("list" as DisplayType);
+    const [display, setDisplay] = useState("list" );
 
 
     return {display, setDisplay};
