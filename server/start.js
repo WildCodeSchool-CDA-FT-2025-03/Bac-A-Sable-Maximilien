1b047e30262818c65ff10b00231babe8a0b72339
Maximilien <maximilien.philippe@protonmail.com>
2025-03-17 19:16:28 +0100

    prettier start.js

--- server/start.js
+++ server/start.js
@@ -1,18 +1,18 @@
-const path = require('node:path');
-const concurrently = require('concurrently');
+const path = require("node:path");
+const concurrently = require("concurrently");
 
 const { result } = concurrently(
-    [
-        'npm:watch-*',
-        { command: 'npx nodemon', name: 'server', cwd: '.'},
-        { command: 'npm run dev', name: 'client', cwd: '../client'},
-    ],
-    {
-        prefix: '',
-        killOthers: ['failure', 'success'],
-        restartTries: 3,
-        cwd: path.resolve(__dirname, 'scripts'),
-    },
+  [
+    "npm:watch-*",
+    { command: "npx nodemon", name: "server", cwd: "." },
+    { command: "npm run dev", name: "client", cwd: "../client" },
+  ],
+  {
+    prefix: "",
+    killOthers: ["failure", "success"],
+    restartTries: 3,
+    cwd: path.resolve(__dirname, "scripts"),
+  },
 );
 
 // result.then(success, failure);
