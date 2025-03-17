c0b264bfa9ff39eee0fd681ce20808115ecd0e82
Maximilien <maximilien.philippe@protonmail.com>
2025-03-17 18:59:54 +0100

    prettier service github

--- server/src/services/github.ts
+++ server/src/services/github.ts
@@ -1,58 +1,59 @@
-import { GitHubRepository, GitHubRepositoryLanguage, Repositories } from "@shared/repository.types"
+import {
+  GitHubRepository,
+  GitHubRepositoryLanguage,
+  Repositories,
+} from "@shared/repository.types";
 import githubFetch from "./http/http_github";
 
 type PartialGitHubRepo = {
-    name: string,
-    private: boolean,
-    url: string,
-    node_id: string,
-    owner: {
-        login: string,
-        id: number,
-    },
-    description: string,
-    created_at: string,
-    language: string,
-}
+  name: string;
+  private: boolean;
+  url: string;
+  node_id: string;
+  owner: {
+    login: string;
+    id: number;
+  };
+  description: string;
+  created_at: string;
+  language: string;
+};
 
 const getLangs = async (repo: PartialGitHubRepo): Promise<GitHubRepository> => {
-    const data = await githubFetch.languages(repo.owner.login, repo.name);
-    const langsOld = await data.json();
-    const langs = Object.entries(langsOld).map(([l, s]) => {
-            return {size: s, node:{name: l}} as GitHubRepositoryLanguage;
-        });
+  const data = await githubFetch.languages(repo.owner.login, repo.name);
+  const langsOld = await data.json();
+  const langs = Object.entries(langsOld).map(([l, s]) => {
+    return { size: s, node: { name: l } } as GitHubRepositoryLanguage;
+  });
 
+  const repos: GitHubRepository = {
+    id: repo.node_id,
+    isPrivate: repo.private,
+    url: repo.url,
+    description: repo.description,
+    createdAt: repo.created_at,
+    primaryLanguage: { name: repo.language },
+    languages: langs,
+    name: repo.name,
+    owner: { id: "" + repo.owner.id, login: repo.owner.login },
+  };
 
-    const repos: GitHubRepository = {
-            id: repo.node_id,
-            isPrivate: repo.private,
-            url: repo.url,
-            description: repo.description,
-            createdAt: repo.created_at,
-            primaryLanguage: {name: repo.language},
-            languages: langs,
-            name: repo.name,
-            owner: {id: ""+repo.owner.id, login: repo.owner.login}
-    };
-
-    return repos;
+  return repos;
 };
 
 const github = {
-    async getRepository(userName: string) : Promise<Repositories> {
+  async getRepository(userName: string): Promise<Repositories> {
+    const results = await githubFetch.repository(userName);
+    const datas = (await results.json()) as PartialGitHubRepo[];
 
-        const results = await githubFetch.repository(userName);
-        const datas = await results.json() as PartialGitHubRepo[];
+    const waiting = [];
 
-        const waiting = [];
-
-        for(const r of datas) {
-            waiting.push(getLangs(r));
-        }
-
-        return await Promise.all(waiting);;
+    for (const r of datas) {
+      waiting.push(getLangs(r));
     }
-}
+
+    return await Promise.all(waiting);
+  },
+};
 
 export default github;
-
--- server/src/services/http/http_github.ts
+++ server/src/services/http/http_github.ts
@@ -1,28 +1,33 @@
 // import { Response } from "express";
 
 const githubFetch = {
-    repository: (userName: string) => {
-        const url = `users/${userName}/repos`
-        return github_fetch(url);
-    },
+  repository: (userName: string) => {
+    const url = `users/${userName}/repos`;
+    return github_fetch(url);
+  },
 
-    languages: async (userName: string, projectName: string) => {
-        const url = `repos/${userName}/${projectName}/languages`;
-        return github_fetch(url);
-    }
+  languages: async (userName: string, projectName: string) => {
+    const url = `repos/${userName}/${projectName}/languages`;
+    return github_fetch(url);
+  },
 };
 
-
 function github_fetch(url: string) {
-    let options = {
-        method: "GET",
-    } as RequestInit;
+  let options = {
+    method: "GET",
+  } as RequestInit;
 
-    if(process.env.GITHUB_API_KEY !== undefined && process.env.GITHUB_API_KEY.length > 0) {
-       options = {...options, headers: {"Authorization": process.env.GITHUB_API_KEY}};
-    }
+  if (
+    process.env.GITHUB_API_KEY !== undefined &&
+    process.env.GITHUB_API_KEY.length > 0
+  ) {
+    options = {
+      ...options,
+      headers: { Authorization: process.env.GITHUB_API_KEY },
+    };
+  }
 
-    return fetch(`https://api.github.com/${url}`, options);
+  return fetch(`https://api.github.com/${url}`, options);
 }
 
 export default githubFetch;
