1b58c36503cce57c6344f3ccbbaaeedb5a126208
Maximilien <maximilien.philippe@protonmail.com>
2025-03-18 08:26:16 +0100

    @client search bar add user at github service

--- client/src/components/BarSearchRepo/BarSearchRepo.tsx
+++ client/src/components/BarSearchRepo/BarSearchRepo.tsx
@@ -1,17 +1,33 @@
+import { useRef } from 'react';
 import './BarSearchRepo.css'
 import { FaGithub, FaSearch } from "react-icons/fa";
+import useGithub from '@/services/github.service';
 
 const BarSearchRepo = () => {
+    const {users, setUsers} = useGithub();
+    const input = useRef<HTMLInputElement>(null);
+
+    const addUser = () => {
+        const name = input.current ? input.current.value : "";
+        if(name.length > 0 && !users.includes(name)) {
+
+            if(input.current !== null) {
+                setUsers([...users, name ]);
+                input.current.value = "";
+            }
+        }
+    }
 
     return (
 
         <div className='bar-search'>
 
             <FaGithub size="1.7rem"/>
-            <input className="bar-input" type="text"/>
-            <div className="bar-enter">
+            <input ref={input} className="bar-input" type="text"/>
+            <div className="bar-enter" onClick={()=>addUser()}>
                 <FaSearch size="1.3rem"/>
             </div>
+
         </div>
     )
 }
